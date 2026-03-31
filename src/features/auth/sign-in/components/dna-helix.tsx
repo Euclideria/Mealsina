79e6269bab7f19fa82b4dd53e0326b248609309e
Mealsina Dev <dev@mealsina.local>
2026-03-31 04:14:54 +0200

    feat: DNA double helix with rotateY so balls stay perfectly round

--- src/features/auth/sign-in/components/dna-helix.tsx
+++ src/features/auth/sign-in/components/dna-helix.tsx
@@ -4,12 +4,43 @@ export function DNAHelix() {
   return (
     <div className='dna-helix-container'>
       <div id='dna'>
-        <div></div><div></div><div></div><div></div><div></div>
-        <div></div><div></div><div></div><div></div><div></div>
-        <div></div><div></div><div></div><div></div><div></div>
-        <div></div><div></div><div></div><div></div><div></div>
-        <div></div><div></div><div></div><div></div><div></div>
-        <div></div><div></div><div></div><div></div><div></div>
+        {/* Strand 1 - 15 bars rotating with rotateY */}
+        <div className='strand strand-1'>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+        </div>
+
+        {/* Strand 2 - 15 bars 180 degrees out of phase */}
+        <div className='strand strand-2'>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+        </div>
       </div>
     </div>
   )

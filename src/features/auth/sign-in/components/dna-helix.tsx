b8dbc0df301a4579df77d0ef918b3f2496ccac32
Mealsina Dev <dev@mealsina.local>
2026-03-31 02:47:24 +0200

    feat: DNA helix with 3D rotating lines and white dots

--- src/features/auth/sign-in/components/dna-helix.tsx
+++ src/features/auth/sign-in/components/dna-helix.tsx
@@ -3,31 +3,10 @@ import './dna-helix.css'
 export function DNAHelix() {
   return (
     <div className='dna-helix-container'>
-      {/* DNA Helix with two intertwined strands */}
       <div className='dna-wrapper'>
-        {/* Strand 1 */}
-        <div className='dna-strand'>
-          {/* Nucleotides (horizontal bars with balls) */}
-          {Array.from({ length: 10 }).map((_, i) => (
-            <div key={`n1-${i}`} className='dna-nucleotide' />
-          ))}
-          {/* Backbone dots */}
-          {Array.from({ length: 11 }).map((_, i) => (
-            <div key={`b1-${i}`} className='dna-backbone strand1' />
-          ))}
-        </div>
-
-        {/* Strand 2 */}
-        <div className='dna-strand'>
-          {/* Nucleotides (horizontal bars with balls) */}
-          {Array.from({ length: 10 }).map((_, i) => (
-            <div key={`n2-${i}`} className='dna-nucleotide' />
-          ))}
-          {/* Backbone dots */}
-          {Array.from({ length: 11 }).map((_, i) => (
-            <div key={`b2-${i}`} className='dna-backbone strand2' />
-          ))}
-        </div>
+        {Array.from({ length: 30 }).map((_, i) => (
+          <div key={i} className='dna-line' />
+        ))}
       </div>
     </div>
   )

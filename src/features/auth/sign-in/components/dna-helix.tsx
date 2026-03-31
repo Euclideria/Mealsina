a55c10b2ae7bc02759c905d7c215942506324d19
Mealsina Dev <dev@mealsina.local>
2026-03-31 04:21:58 +0200

    feat: DNA helix with proper 3D structure

--- src/features/auth/sign-in/components/dna-helix.tsx
+++ src/features/auth/sign-in/components/dna-helix.tsx
@@ -3,44 +3,26 @@ import './dna-helix.css'
 export function DNAHelix() {
   return (
     <div className='dna-helix-container'>
-      <div id='dna'>
-        {/* Strand 1 - 15 bars at different heights */}
-        <div className='strand strand-1'>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
+      <div className='helix-wrapper'>
+        {/* Backbone 1 */}
+        <div className='backbone backbone-1'>
+          {[0, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375].map((y, i) => (
+            <div key={i} className='backbone-dot' style={{ top: `${y}px` }} />
+          ))}
         </div>
-
-        {/* Strand 2 - 15 bars 180 degrees out of phase */}
-        <div className='strand strand-2'>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
-          <div className='bar'></div>
+        {/* Backbone 2 */}
+        <div className='backbone backbone-2'>
+          {[0, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375].map((y, i) => (
+            <div key={i} className='backbone-dot' style={{ top: `${y}px` }} />
+          ))}
         </div>
+        {/* Base pairs */}
+        {[0, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375].map((y, i) => (
+          <div key={i} className='base-pair' style={{ top: `${y}px` }}>
+            <div className='sphere'></div>
+            <div className='sphere'></div>
+          </div>
+        ))}
       </div>
     </div>
   )

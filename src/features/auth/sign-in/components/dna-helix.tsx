1a7a96005516ea0ffa12424463807bbf874114de
Mealsina Dev <dev@mealsina.local>
2026-03-31 02:53:50 +0200

    feat: DNA helix with vertical lines matching reference CSS

--- src/features/auth/sign-in/components/dna-helix.tsx
+++ src/features/auth/sign-in/components/dna-helix.tsx
@@ -4,18 +4,12 @@ export function DNAHelix() {
   return (
     <div className='dna-helix-container'>
       <div className='dna-wrapper'>
-        {/* Strand 1 */}
-        <div className='strand strand-1'>
-          <div></div><div></div><div></div><div></div><div></div>
-          <div></div><div></div><div></div><div></div><div></div>
-          <div></div><div></div><div></div><div></div><div></div>
-        </div>
-        {/* Strand 2 */}
-        <div className='strand strand-2'>
-          <div></div><div></div><div></div><div></div><div></div>
-          <div></div><div></div><div></div><div></div><div></div>
-          <div></div><div></div><div></div><div></div><div></div>
-        </div>
+        <div></div><div></div><div></div><div></div><div></div>
+        <div></div><div></div><div></div><div></div><div></div>
+        <div></div><div></div><div></div><div></div><div></div>
+        <div></div><div></div><div></div><div></div><div></div>
+        <div></div><div></div><div></div><div></div><div></div>
+        <div></div><div></div><div></div><div></div><div></div>
       </div>
     </div>
   )

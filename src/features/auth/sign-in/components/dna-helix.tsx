ba6b48a947deab3bd981d5bbffd5bcfa56bc7233
Mealsina Dev <dev@mealsina.local>
2026-03-31 04:18:00 +0200

    feat: DNA helix with bars at different heights

--- src/features/auth/sign-in/components/dna-helix.tsx
+++ src/features/auth/sign-in/components/dna-helix.tsx
@@ -4,42 +4,42 @@ export function DNAHelix() {
   return (
     <div className='dna-helix-container'>
       <div id='dna'>
-        {/* Strand 1 - 15 bars rotating with rotateY */}
+        {/* Strand 1 - 15 bars at different heights */}
         <div className='strand strand-1'>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
         </div>
 
         {/* Strand 2 - 15 bars 180 degrees out of phase */}
         <div className='strand strand-2'>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
-          <div className='bar'><div className='ball-top' /><div className='ball-bottom' /></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
         </div>
       </div>
     </div>
